0ccba192b908311775fb590874dc4c3b68d51fdd
Bigstargithub <qufquf2004@hanmail.net>
2021-09-15 10:25:51 +0900

    공지사항 슬라이드 작업 전

--- js/main.js
+++ js/main.js
@@ -13,4 +13,35 @@ searchInput.addEventListener('focus', function() {
 searchInput.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInput.setAttribute('placeholder','');
-})
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {
+  console.log(window.scrollY);
+  if(window.scrollY > 500)
+  {
+    gsap.to(badgeEl, .3, {
+      opacity: 0,
+      display: 'none'
+    })
+  }
+  else 
+  {
+    gsap.to(badgeEl, .3, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
+// _.throttle(함수, 시간) - 많은
+// gsap.to(요소, 지속시간, 옵션) - 애니메이션 동작
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  gsap.to(fadeEl,1,{
+    delay: (index+1)*.4,
+    opacity: 1
+  })
+});
